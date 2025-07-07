b70535bb7d811c28446100fd41483aaa83eb2ddf
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-07 17:04:26 +0100

    cpp05 - ex01 - setup

---
+++ ex01/inc/Bureaucrat.hpp
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 12:08:08 by lude-bri          #+#    #+#             */
+/*   Updated: 2025/07/07 13:31:33 by lude-bri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+//LIBRARIES
+#include <iostream>
+
+//DEBUG
+#ifdef DEBUG
+# define DEBUG_MSG(x) std::cout << "[DEBUG]: " << x << std::endl
+#else
+# define DEBUG_MSG(x)
+#endif
+
+//COLORS
+# define RED     "\033[31m"
+# define GREEN   "\033[32m"
+# define YELLOW  "\033[33m"
+# define BLUE    "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN    "\033[36m"
+# define RESET   "\033[0m"
+# define BOLD    "\033[1m"
+# define BLINK    "\033[5m" 
+# define REVERSE  "\033[7m"
+
+//CLASSES
+class Bureaucrat {
+
+private:
+	const std::string	_name;
+	int					_grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(const std::string &name, int grade);
+	Bureaucrat(const Bureaucrat &copy);
+	Bureaucrat &operator=(const Bureaucrat &copy);
+	~Bureaucrat();
+
+	//validation
+	int			isValid(int grade) const;
+	void		verifyGrade(int grade) const;
+
+	//getters
+	std::string	getName() const;
+	int			getGrade() const;
+
+	//exceptions
+	void		GradeTooHighException() const;
+	void		GradeTooLowException() const;
+
+	//grades control
+	void		incrementGrade();
+	void		decrementGrade();
+};
+
+//overload
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &b);
---
+++ ex01/src/Bureaucrat.cpp
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/07 12:10:49 by lude-bri          #+#    #+#             */
+/*   Updated: 2025/07/07 13:36:01 by lude-bri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Bureaucrat.hpp"
+
+//Default constructor
+Bureaucrat::Bureaucrat() {
+	std::cout << "A default Bureaucrat was build" << std::endl;
+}
+
+//Constructor
+Bureaucrat::Bureaucrat(const std::string &name, int grade) : _name(name) {
+	DEBUG_MSG("Bureaucrat's Constructor was called\n");
+	_grade = isValid(grade);
+
+	std::cout << "Bureaucrat " << _name << "'s grade is " << getGrade() << std::endl;
+}
+
+//Copy Constructor
+Bureaucrat::Bureaucrat(const Bureaucrat &copy) {
+	DEBUG_MSG("Bureaucrat's Copy Constructor was called\n");
+	*this = copy;
+}
+
+//Copy operator = assignment
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &copy) {
+	DEBUG_MSG("Bureaucrat's Operator Assignment = was called\n");
+	return *this;
+}
+
+//Copy operator << assignment
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &b) {
+	out << b.getName() << ", bureaucrat grade " << b.getGrade() << std::endl;
+	return out;
+}
+
+//Destructor
+Bureaucrat::~Bureaucrat(){
+	DEBUG_MSG("Bureaucrat's Destructor was called\n");
+}
+
+//Validation of grade outbounds
+int		Bureaucrat::isValid(int grade) const {
+	if (grade < 1)
+		GradeTooHighException();
+	if (grade > 150)
+		GradeTooLowException();
+	return grade;
+}
+
+//getters
+std::string	Bureaucrat::getName() const {
+	// std::cout << "Bureaucrat name's is : " << _name << std::endl;
+	return (_name);
+}
+
+int	Bureaucrat::getGrade() const { 
+	// std::cout << "Bureaucrat grade's is : " << _grade << std::endl;
+	return (_grade);
+}
+
+
+//Exceptions LOW
+void		Bureaucrat::GradeTooLowException() const {
+	throw std::runtime_error(_name + ": grade is too low");
+}
+
+//Exceptions HIGH 
+void		Bureaucrat::GradeTooHighException() const {
+	throw std::runtime_error(_name + ": grade is too high");
+}
+
+//grades control
+void	Bureaucrat::incrementGrade() {
+	if (_grade <= 1)
+		GradeTooHighException();
+	else
+		--_grade;
+}
+
+void	Bureaucrat::decrementGrade() {
+	if (_grade >= 150)
+		GradeTooLowException();
+	else
+		++_grade;
+}
