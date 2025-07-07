779b40eb5cbf1dea5957b778228fc152cf250aeb
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-07 17:56:35 +0100

    cpp05 - ex01 - setup

--- ex01/inc/Form.hpp
+++ ex01/inc/Form.hpp
@@ -6,26 +6,12 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 17:04:37 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/07 17:27:53 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/07 17:56:31 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include "Bureaucrat.hpp"
-
-
-//Let's Create a Form class that has
-//
-//A constant name
-//A boolean indicating whether it is signed
-//A const grade required to sign it
-//A const grade required to executed it
-//
-//ALL PRIVATE
-
-//Form follow the same rules of the Bureaucrat -> GradeTooHighException GradeTooLowException
-//write getters for all attributes
-//overload << to print all attributes
 
 //Add a beSigned(const Bureaucrat &b)
 
@@ -47,9 +33,18 @@ public:
 	~Form();
 
 	//Exceptions
+	void			GradeTooLowException() const;
+	void			GradeTooHighException() const;
+
 	//getters
+	std::string		getName();
+	bool			getFormState();
+	int				getGradeToSign();
+	int				getGradeToExecute();
+
 	//Sign
+	void			beSigned(const Bureaucrat &b);
 };
 
 //operator overload
-std::ostream &operator<<(std::ostream &out, const Form &f);
+std::ostream &operator<<(std::ostream &out, Form &f);
--- ex01/src/Form.cpp
+++ ex01/src/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 17:04:31 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/07 17:38:19 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/07 17:54:12 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,34 @@ Form::~Form() {
 }
 
 //Operator Overload
+std::ostream &operator<<(std::ostream &out, Form &f) {
+
+	out << "This is the Form " << f.getName() << std::endl;
+	out << "with this attributes: " << std::endl;
+	out << "1. Form's State -> Signed: " << f.getFormState() << std::endl;
+	out << "2. Required Grade to Sign: " << f.getGradeToSign() << std::endl;
+	out << "3. Required Grade to Execute: " << f.getGradeToExecute() << std::endl;
+	return out;
+}
+
 //Getters
+std::string		Form::getName() { return _name; }
+bool			Form::getFormState() { return _isSigned; }
+int				Form::getGradeToSign() {return _gradeToSign; }
+int				Form::getGradeToExecute() {return _gradeToExecute; }
+
 //Exceptions
+void	Form::GradeTooLowException() const {
+	throw std::runtime_error(_name + ": grade is too low");
+}
+
+void	Form::GradeTooHighException() const {
+	throw std::runtime_error(_name + ": grade is too high");
+}
+
+//Sign
+void	Form::beSigned(const Bureaucrat &b) {
+
+
+}
+
