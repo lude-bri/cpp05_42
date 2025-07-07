aee7aadc72acac82c0ed9024e799f7c8473bf2fb
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-07 19:10:51 +0100

    cpp05 - ex02 - setup

--- ex02/inc/AForm.hpp
+++ ex02/inc/AForm.hpp
@@ -6,8 +6,48 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 18:48:17 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/07 18:48:27 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/07 19:09:54 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
 
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+//CLASS
+class AForm {
+
+private:
+
+	const std::string	_name;
+	bool				_isSigned;
+	const int			_gradeToSign;
+	const int			_gradeToExecute;
+
+public:
+	AForm();
+	AForm(const std::string &name);
+	AForm(const AForm &copy);
+	AForm &operator=(const AForm &copy);
+	~AForm();
+
+	//Exceptions
+	void			GradeTooLowException() const;
+	void			GradeTooHighException() const;
+
+	//getters
+	std::string		getName();
+	bool			getFormState();
+	int				getGradeToSign();
+	int				getGradeToExecute();
+
+	//Sign
+	void			beSigned(Bureaucrat &b);
+
+	//Execute
+	virtual void			execute(Bureaucrat const &executor) = 0;
+};
+
+//operator overload
+std::ostream &operator<<(std::ostream &out, AForm &f);
