fc29ed8dc4482797062e98d421079fb588860441
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-07 17:33:33 +0100

    cpp05 - ex01 - setup

--- ex01/src/Form.cpp
+++ ex01/src/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 17:04:31 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/07 17:28:04 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/07 17:33:19 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,12 @@ Form::Form(std::string &name) : _name(name), _isSigned(false) {
 }
 
 //Copy Constructor
-
+Form::Form(const Form &copy) 
+	: _name(copy._name), _isSigned(copy._isSigned),
+	_gradeToSign(copy._gradeToSign), _gradeToExecute(copy._gradeToExecute)
+{
+	DEBUG_MSG("A Form Copy Constructor was build\n");
+}
 
 //Operator Assignmed
 //Destructor
