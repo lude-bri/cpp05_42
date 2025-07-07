f3aa12b721b5f5143e7e359f22204a2c6e8a832b
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-07 17:28:05 +0100

    cpp05 - ex01 - setup

--- ex01/inc/Form.hpp
+++ ex01/inc/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 17:04:37 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/07 17:20:03 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/07 17:27:53 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,12 @@
 //
 //ALL PRIVATE
 
+//Form follow the same rules of the Bureaucrat -> GradeTooHighException GradeTooLowException
+//write getters for all attributes
+//overload << to print all attributes
+
+//Add a beSigned(const Bureaucrat &b)
+
 //CLASS
 class Form {
 
@@ -33,4 +39,17 @@ private:
 	const int			_gradeToSign = 75;
 	const int			_gradeToExecute = 120;
 
+public:
+	Form();
+	Form(std::string &name);
+	Form(const Form &copy);
+	Form &operator=(const Form &copy);
+	~Form();
+
+	//Exceptions
+	//getters
+	//Sign
 };
+
+//operator overload
+std::ostream &operator<<(std::ostream &out, const Form &f);
--- ex01/src/Form.cpp
+++ ex01/src/Form.cpp
@@ -6,14 +6,25 @@
 /*   By: lude-bri <lude-bri@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/07 17:04:31 by lude-bri          #+#    #+#             */
-/*   Updated: 2025/07/07 17:25:29 by lude-bri         ###   ########.fr       */
+/*   Updated: 2025/07/07 17:28:04 by lude-bri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Form.hpp"
 
 //Default Constructor
+Form::Form() : _isSigned(false) {
+	DEBUG_MSG("A Default Form was build\n");
+}
+
+//Parametric Constructor
+Form::Form(std::string &name) : _name(name), _isSigned(false) {
+	DEBUG_MSG("A Form was build\n");
+}
+
 //Copy Constructor
+
+
 //Operator Assignmed
 //Destructor
 //Operator Overload
