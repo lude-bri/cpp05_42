74ba73a780339e862f14eb52d60a8869430d51a4
Luigi Piantavinha <szochoralewizc@hotmail.com>
2025-07-09 16:18:16 +0100

    cpp05 - ex02 - OK

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -33,35 +33,114 @@ int main(void) {
 
 			std::cout << form << std::endl;
 
-			// Tentativa de assinatura com grau insuficiente
 			novice.signForm(form);
-
-			// Assinatura válida
 			gardener.signForm(form);
 
-			// Execução com grau insuficiente
 			novice.executeForm(form);
-
-			// Execução válida
 			gardener.executeForm(form);
-
-			// Criar o arquivo
-			form.action();
-
+			
 			// Verificar se o arquivo foi criado
 			std::ifstream file("home_shrubbery");
 			if (file.good()) {
-				std::cout << "Arquivo criado com sucesso!" << std::endl;
+				std::cout << "File was created!!" << std::endl;
 			} else {
-				std::cout << "Erro: arquivo não criado!" << std::endl;
+				std::cout << "Error: file was not created" << std::endl;
 			}
 		} catch (std::exception &e) {
 			std::cerr << "Erro: " << e.what() << std::endl;
 		}
 	}
-	// printTestHeader("Test 01. ");
-	// printTestHeader("Test 02. ");
-	// printTestHeader("Test 03. ");
-	// printTestHeader("Test 04. ");
+	printTestHeader("Test 01. RobotomyRequestForm");
+	{
+		//SEVERANCE FLOOR!
+		Bureaucrat Irving("Irving", 100);
+		Bureaucrat Mark("Mark", 1);
+		Bureaucrat Dylan("Dylan", 42);
+		Bureaucrat Helly("Helly", 55);
+
+		RobotomyRequestForm form("TopSecret");
+		
+		//show form
+		std::cout << form << std::endl;
+
+		//trying to sign form
+		Irving.signForm(form); //no
+		Mark.signForm(form); //yes
+		Dylan.signForm(form); //yes
+		Helly.signForm(form); //yes
+		
+		//make irving great to sign
+		for (int i = 0; i < 99; i++)
+			Irving.incrementGrade();
+
+		//new irving grade
+		std::cout << Irving << std::endl;
+
+		//try to sign
+		Irving.signForm(form);
+
+		std::cout << std::endl;
+		//Execute Form
+		Irving.executeForm(form);
+		Mark.executeForm(form);
+		Dylan.executeForm(form);
+		Helly.executeForm(form);
+	}
+	printTestHeader("Test 02. PresidentialPardonForm");
+	{
+		Bureaucrat Irving("Irving", 100);
+		Bureaucrat Mark("Mark", 1);
+		Bureaucrat Dylan("Dylan", 42);
+		Bureaucrat Helly("Helly", 55);
+
+		PresidentialPardonForm form("TopSecret");
+		
+		//show form
+		std::cout << form << std::endl;
+
+		//trying to sign form
+		Irving.signForm(form); //no
+		Mark.signForm(form); //yes
+		Dylan.signForm(form); //no
+		Helly.signForm(form); //no
+		
+		//make irving great to sign
+		for (int i = 0; i < 99; i++)
+			Irving.incrementGrade();
+
+		//new irving grade
+		std::cout << Irving << std::endl;
+
+		//try to sign
+		Irving.signForm(form);
+
+		std::cout << std::endl;
+		//Execute Form
+		Irving.executeForm(form);
+		Mark.executeForm(form);
+		Dylan.executeForm(form);
+		Helly.executeForm(form);
+	}
+	printTestHeader("Test 03. ShrubberyCreationForm - Error Cases");
+	{
+		try {
+			
+			Bureaucrat littleBoy("Junior", 146);
+			ShrubberyCreationForm form("Forest");
+
+			std::cout << form << std::endl;
+
+			littleBoy.executeForm(form);
+			littleBoy.signForm(form);
+
+			Bureaucrat boy("Mid", 138);
+			boy.signForm(form);
+			boy.executeForm(form);
+			
+		}
+		catch (std::exception const &e) {
+			std::cerr << "Error: " << e.what() << std::endl;
+		}
+	}
 }
 
